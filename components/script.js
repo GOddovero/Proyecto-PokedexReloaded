d2b271c5a4184cc2623d02f031e8e7f5e60c3530
Gaspar Oddovero Herrera <gaspyoddovero742@gmail.com>
2024-04-27 09:27:05 -0300

    img pokes

--- components/script.js
+++ components/script.js
@@ -14,7 +14,30 @@ function generateRandomImageNumbers() {
 
     return imageNumbers;
 }
-
+function generateRandomNumber(){
+    kanto = Math.floor(Math.random() * 151) + 1;
+    johto = Math.floor(Math.random() * 100) + 1;
+    hoenn = Math.floor(Math.random() * 135) + 1;
+    sinnoh = Math.floor(Math.random() * 107) + 1;
+    teselia = Math.floor(Math.random() * 169) + 1;
+    kalos = Math.floor(Math.random() * 100) + 1;
+    alola = Math.floor(Math.random() * 125) + 1;
+    galar = Math.floor(Math.random() * 110) + 1;
+    hisui = Math.floor(Math.random() * 30) + 1;
+    star = Math.floor(Math.random() * 56) + 1;
+    return {
+        kanto: kanto,
+        johto: johto,
+        hoenn: hoenn,
+        sinnoh: sinnoh,
+        teselia: teselia,
+        kalos: kalos,
+        alola: alola,
+        galar: galar,
+        hisui: hisui,
+        star: star
+    };  
+}
 function getColorForNumber(number) {
     // Reglas para determinar el color basado en el número
     if ([1, 6, 9, 12, 16, 19, 22, 25, 28].includes(number)) {
@@ -32,6 +55,7 @@ function getColorForNumber(number) {
     }
 }
 
+
 function loadRandomImages() {
     var randomImageNumbers = generateRandomImageNumbers();
     var tarjetaStarters = document.querySelectorAll(".divStarters .TarjetaStarters");
@@ -40,13 +64,87 @@ function loadRandomImages() {
         var randomImageNumber = randomImageNumbers[index];
         var color = getColorForNumber(randomImageNumber);
         var imageUrl = "img/starters/a- (" + randomImageNumber + ").gif";
-
-        // Establece el color de fondo de la tarjeta y la imagen
         tarjeta.style.backgroundColor = color;
         tarjeta.querySelector("img").src = imageUrl;
     });
+    // Kanto
+    var numeroRegiones = generateRandomNumber();
+    console.log(numeroRegiones.kanto);
+    var imgUrlKanto = "img/kanto/kanto- (" + numeroRegiones.kanto + ").gif";
+    var tarjetaRegion = document.querySelector("#TarjetaRegionKanto");
+    tarjetaRegion.querySelector("img").src = imgUrlKanto;
+    console.log(imgUrlKanto);
+
+
+    // Johto
+    console.log(numeroRegiones.johto);
+    var imgUrlJohto = "img/johto/johto- (" + numeroRegiones.johto + ").gif";
+    var tarjetaRegionJohto = document.querySelector("#TarjetaRegionJohto");
+    tarjetaRegionJohto.querySelector("img").src = imgUrlJohto;
+    console.log(imgUrlJohto);
+
+    // Hoenn
+    console.log(numeroRegiones.hoenn);
+    var imgUrlHoenn = "img/hoenn/hoenn- (" + numeroRegiones.hoenn + ").gif";
+    var tarjetaRegionHoenn = document.querySelector("#TarjetaRegionHoenn");
+    tarjetaRegionHoenn.querySelector("img").src = imgUrlHoenn;
+    console.log(imgUrlHoenn);
+    
+
+    // Sinnoh
+    console.log(numeroRegiones.sinnoh);
+    var imgUrlSinnoh = "img/sinnoh/sinnoh- (" + numeroRegiones.sinnoh + ").gif";
+    var tarjetaRegionSinnoh = document.querySelector("#TarjetaRegionSinnoh");
+    tarjetaRegionSinnoh.querySelector("img").src = imgUrlSinnoh;
+    console.log(imgUrlSinnoh);
+
+    // Teselia
+    console.log(numeroRegiones.teselia);
+    var imgUrlTeselia = "img/teselia/teselia- (" + numeroRegiones.teselia + ").gif";
+    var tarjetaRegionTeselia = document.querySelector("#TarjetaRegionTeselia");
+    tarjetaRegionTeselia.querySelector("img").src = imgUrlTeselia;
+    console.log(imgUrlTeselia);
+
+    // Kalos
+    console.log(numeroRegiones.kalos);
+    var imgUrlKalos = "img/kalos/kalos- (" + numeroRegiones.kalos + ").gif";
+    var tarjetaRegionKalos = document.querySelector("#TarjetaRegionKalos");
+    tarjetaRegionKalos.querySelector("img").src = imgUrlKalos;
+    console.log(imgUrlKalos);
+
+    // Alola
+    console.log(numeroRegiones.alola);
+    var imgUrlAlola = "img/alola/alola- (" + numeroRegiones.alola + ").gif";
+    var tarjetaRegionAlola = document.querySelector("#TarjetaRegionAlola");
+    tarjetaRegionAlola.querySelector("img").src = imgUrlAlola;
+    console.log(imgUrlAlola);
+
+    // Galar
+    console.log(numeroRegiones.galar);
+    var imgUrlGalar = "img/galar/galar- (" + numeroRegiones.galar + ").gif";
+    var tarjetaRegionGalar = document.querySelector("#TarjetaRegionGalar");
+    tarjetaRegionGalar.querySelector("img").src = imgUrlGalar;
+    console.log(imgUrlGalar);
+
+    // Hisui
+    console.log(numeroRegiones.hisui);
+    var imgUrlHisui = "img/hisui/hisui- (" + numeroRegiones.hisui + ").gif";
+    var tarjetaRegionHisui = document.querySelector("#TarjetaRegionHisui");
+    tarjetaRegionHisui.querySelector("img").src = imgUrlHisui;
+    console.log(imgUrlHisui);
+
+    // Star
+    console.log(numeroRegiones.star);
+    var imgUrlStar = "img/star/star- (" + numeroRegiones.star + ").gif";
+    var tarjetaRegionStar = document.querySelector("#TarjetaRegionStar");
+    tarjetaRegionStar.querySelector("img").src = imgUrlStar;
+    console.log(imgUrlStar);
+
+
 }
 
 window.onload = function() {
     loadRandomImages();
+    generateRandomNumber();
 };
+
